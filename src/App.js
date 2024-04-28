4d95fb80176fb2ac36f6eb2a3fa3ef46b972d545
Tisha Yadav <tyadhuvanshi1408@gmail.com>
2024-04-28 15:02:14 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import { HeartIcon, SpinnerIcon } from "./utils/icons";
 
 function App() {
+  const [liked, setLiked] = useState(false);
+  const [isFetching, setIsFetching] = useState(false);
+  const [error, setError] = useState(null);
+
+  const handleLikeUnlike = async () => {
+    setIsFetching(true);
+    setError("");
+
+    try {
+      const response = await fetch("https://www.greatfrontend.com/api/questions/like-button", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          action: liked ? "unlike" : "like",
+        }),
+      });
+
+      if (response.status >= 200 && response.status < 300) {
+        setLiked(!liked);
+      } else {
+        const res = await response.json();
+        setError(res.message);
+        return;
+      }
+    } finally {
+      setIsFetching(false);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button
+        disabled={isFetching}
+        className={`likeBtn ${liked ? "liked" : ""}`}
+        onClick={handleLikeUnlike}
+      >
+        {isFetching ? <SpinnerIcon /> : <HeartIcon />}
+        {liked ? "Liked" : "Like"}
+      </button>
+      {/* error handling */}
+      {error && <div className="error">{error}</div>}
     </div>
   );
 }
